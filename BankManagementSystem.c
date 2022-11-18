6d71c65095ca32f9e55f44dc48ecd5ec8dd2ecdf
Brent1217 <108774549+Brent1217@users.noreply.github.com>
2022-11-18 15:40:28 -0800

    Updating

--- BankManagementSystem.c
+++ BankManagementSystem.c
@@ -14,7 +14,6 @@ struct{
     int account_number, age;
     double phone;
     char *citizenship;
-    char *account_type;
     char *address;
     float checkings;
     float savings;
@@ -22,26 +21,36 @@ struct{
     struct date deposit;
     struct date withdraw;
     struct date date_of_birth;
-} add, update, rem, transaction, check, display; //deposit and withdraw are transaction only not add or remove
+} add, rem, transaction, check; 
 
 
 void create_new_account() { //creates new account and puts it in the file
-    FILE *infile;
-    infile = fopen("Bank.Database", "w");
-    int choice;
-    printf("Create New Account\n");
-    printf("Please Enter The Account Number\n");
-    scanf("%d\n", &check.account_number);
-    char line[100000];
-    while (fgets(line, sizeof(line), infile)) {
-        if (check.account_number != add.account_number) {
-            printf("Ready to create account!\n");
+            FILE *database = fopen("Bank.Database", "a+"); 
+            if (database == NULL) {
+                perror("Error: \n");
+                return(-1);
+            }
+            int choice; //to store the choice of the user
+            char *to_store_new_line;
+            printf("Please Enter The Account Number\n");
+            scanf("%d\n", &check.account_number); //input to check a number
+            char *string; //for getline
+            int size = 230; //for getline
+            string = (char *)malloc(size); //for getline
+            while(getline(&string, &size, database)) {
+                if (check.account_number == add.account_number) {
+                    printf("Account Number Already In Use\n");
+                    //if the input number is already there
+                    return 0;
+                }
+            }
+            add.account_number = check.account_number; //if not found yet, add.account_number is now 
             printf("Enter Name:  \n");
             scanf("%s\n", add.name);
             printf("Please Enter Birth Date In Format (Month/Date/Year):\n");
             scanf("%d/%d/%d\n", &add.date_of_birth.month, &add.date_of_birth.day, &add.date_of_birth.year);
             printf("Enter Age:  \n");
-            scanf("%d\n", add.age);
+            scanf("%d\n", &add.age);
             printf("Enter Phone Number:  \n");
             scanf("%lf\n", &add.phone);
             printf("Enter Address:  ");
@@ -60,53 +69,49 @@ void create_new_account() { //creates new account and puts it in the file
                 add.savings += transaction.checkings;
             }
             else {
-                printf("Please try again\n");
+                return 0;
             }
-            printf("Enter Account Type:  ");
-            scanf("%s\n", add.account_type);
             printf("Enter New Pin:  ");
-            scanf("%d\n", add.pin);
-            fprintf(infile, "Account Number: %d | Name: %s | Email: %s | Birth Date: %d/%d/%d | Age: %d | Phone Number: %d | Address: %s | Citizenship: %s | Savings: %f | Checkings: %f | Account Type: %s | Pin: %d\n", &add.account_number,add.name,add.email,
+            scanf("%d\n", &add.pin);
+            fprintf(database, "Account Number: %d | Name: %s | Email: %s | Birth Date: %d/%d/%d | Age: %d | Phone Number: %d | Address: %s | Citizenship: %s | Savings: %f | Checkings: %f | Account Type: %s | Pin: %d\n", &check.account_number,add.name,add.email,
             
-            &add.date_of_birth.month,&add.date_of_birth.day,&add.date_of_birth.year,&add.age,&add.phone,add.address,add.citizenship,&add.checkings, &add.savings
-            ,add.account_type, &add.pin); //store the customer information into the file
+            &add.date_of_birth.month,&add.date_of_birth.day,&add.date_of_birth.year,&add.age,&add.phone,add.address,add.citizenship,&add.checkings, &add.savings, &add.pin); //store the customer information into the file
             printf("Account Successfully Created!\n");
             
-            
-        }
-        else if (check.account_number == add.account_number) {
-            printf("This account is already in use.");
-        }
-        else {
-            printf("ERROR");
-        }
-    }
-    fclose(infile);
+
+    
+    fclose(database);
 }
 
-void delete_customer(void) {
-    FILE *old_file;
-    FILE *new_file;
-    old_file = fopen("Bank.Database", "r");
-    new_file = fopen("New.Bank.Database", "w");
+void delete_customer() { //tested
+    FILE *database, *new_file;
+    database = fopen("Bank.Database", "w+");
+    new_file = fopen("Updated.Bank.Database", "w");
+    if (database == NULL) {
+        perror("Error: \n");
+        return(-1);
+    }
     printf("Delete Account\n");
     printf("Please Enter Account Number To Delete \n");
-    scanf("%d\n", &rem.account_number); //store the account number to remove
-    char line[100000];
-    while (fgets(line, sizeof(line), old_file)) {
+    scanf("%d\n", &rem.account_number); //when it inputs a value to remove an account, it looks into a file that does not exist
+    char *string;
+    int size = 10;
+    string = (char *)malloc(size);
+    while(getline(&string, &size, database)) {
                 if (rem.account_number != add.account_number) { //as we go down the line and don't see the acct num to rem
                     fprintf(new_file, "Account Number: %d | Name: %s | Email: %s | Birth Date: %d/%d/%d | Age: %d | Phone Number: %d | Address: %s | Citizenship: %s | Savings: %f | Checkings: %f | Account Type: %s | Pin: %d\n",
                     &add.account_number,add.name,add.email,
                     &add.date_of_birth.month,
                     &add.date_of_birth.day,&add.date_of_birth.year,&add.age,&add.phone,add.address,add.citizenship,&add.savings, &add.checkings,
-                    add.account_type, &add.pin);
+                    &add.pin);
                 }
             }
-    printf("awe");
-    fclose(old_file);
+    printf("Successfully Deleted");
+    fclose(database);
     fclose(new_file);
-    remove("Bank.Database");
-    rename("New.Bank.Database", "Bank.Database"); 
+    //remove("Bank.Database");
+    //rename("New.Bank.Database", "Bank.Database"); 
+    database = new_file;
 }
 
 int trans() { //when you get the pin and acct num wrong it just exits
@@ -114,17 +119,21 @@ int trans() { //when you get the pin and acct num wrong it just exits
     int option;
     int deposit = 0;
     int withdraw = 1;
-    FILE *infile;
-    FILE *new_file;
-    infile = fopen("Bank.Database", "r");
-    new_file = fopen("New.Bank.Database", "w");
+    FILE *old_file, *new_file;
+    old_file = fopen("Bank.Database", "r");
+    new_file = fopen("Updated.Bank.Database", "w");
+    if (old_file == NULL) {
+        perror("Error: \n");
+        return(-1);
+    }
     printf("Please Enter Account Number:  \n");
     scanf("%d\n", &transaction.account_number);
     printf("Please Enter Your Pin\n");
     scanf("%d\n", &transaction.pin);
-    char line[100];
-    if (transaction.pin == add.pin) {
-        while(fgets(line, sizeof(line), infile)) { //keep looping until it hits the last line
+    char *string;
+    int size = 10;
+    string = (char *)malloc(size);
+    while(getline(&string, &size, old_file) != EOF) { //keep looping until it hits the last line
                     if (transaction.account_number == add.account_number && transaction.pin == add.pin) {
                         printf("Enter 1 for checkings and 2 for savings.\n"); //make this so if account type if checkings or savings
                         scanf("%d\n", &option);//Another If Else Statement For Checking Or Savings
@@ -140,7 +149,7 @@ int trans() { //when you get the pin and acct num wrong it just exits
                                 &add.account_number,add.name,add.email,
                                 &add.date_of_birth.month,
                                 &add.date_of_birth.day,&add.date_of_birth.year,&add.age,&add.phone,add.address,add.citizenship,&add.savings, &add.checkings,
-                                add.account_type, &add.pin); 
+                                &add.pin); 
                                 printf("Successfully Deposited %d\n", transaction.checkings);
                             }
                                 else if (reply == withdraw) {
@@ -151,7 +160,7 @@ int trans() { //when you get the pin and acct num wrong it just exits
                                 &add.account_number,add.name,add.email,
                                 &add.date_of_birth.month,
                                 &add.date_of_birth.day,&add.date_of_birth.year,&add.age,&add.phone,add.address,add.citizenship,&add.savings, &add.checkings,
-                                add.account_type, &add.pin); 
+                                &add.pin); 
                                 printf("Successfully Withdrew %d\n", transaction.checkings);
                                 
                         }
@@ -168,7 +177,7 @@ int trans() { //when you get the pin and acct num wrong it just exits
                                     &add.account_number,add.name,add.email,
                                     &add.date_of_birth.month,
                                     &add.date_of_birth.day,&add.date_of_birth.year,&add.age,&add.phone,add.address,add.citizenship,&add.savings, &add.checkings,
-                                    add.account_type, &add.pin); 
+                                    &add.pin); 
                                     printf("Successfully Deposited %d\n", transaction.savings);
                             }
                                 else if (reply == withdraw) {
@@ -179,7 +188,7 @@ int trans() { //when you get the pin and acct num wrong it just exits
                                     &add.account_number,add.name,add.email,
                                     &add.date_of_birth.month,
                                     &add.date_of_birth.day,&add.date_of_birth.year,&add.age,&add.phone,add.address,add.citizenship,&add.savings, &add.checkings,
-                                    add.account_type, &add.pin); 
+                                    &add.pin); 
                                     printf("Successfully Withdrew %d\n", transaction.savings);
                         }
                                 else {
@@ -192,18 +201,16 @@ int trans() { //when you get the pin and acct num wrong it just exits
                                 &add.account_number,add.name,add.email,
                                 &add.date_of_birth.month,
                                 &add.date_of_birth.day,&add.date_of_birth.year,&add.age,&add.phone,add.address,add.citizenship,&add.savings, &add.checkings,
-                                add.account_type, &add.pin);
+                                &add.pin);
                         }
             }
-    }}
-    else {
-        printf("Wrong Pin");
-    }    
-        fclose(infile);
-        fclose(new_file);
-        remove("Bank.Database");
-        rename("New.Bank.Database", "Bank.Database");
-
+    }
+    fclose(old_file);
+    fclose(new_file);
+    //remove("Bank.Database"); //set database or Bank.database equal to the new file
+    //rename("New.Bank.Database", "Bank.Database");
+    old_file = new_file;
+    remove(new_file);
 }
 
 
@@ -214,11 +221,11 @@ void close(void) {
 
 
 
-int main(void) {
+int main(void) { //make it so each function will use the same file
     int choice;
     printf("Welcome To The Bank Management System!\n\n");
     printf("Please choose:\n1 for create new account\n2 for deleting an account\n3 for transactions\n4 to close!\n");
-    scanf("%d\n", &choice);
+    scanf("%d\n", &choice); 
     switch(choice) {
         case 1: create_new_account();
         break;
@@ -234,4 +241,3 @@ int main(void) {
     
     }
 }
-
